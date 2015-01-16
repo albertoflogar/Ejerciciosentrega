da5ae59d9548b3e12711b81fbdd48cc40e59f993
albertoflogar <albertoflogar@gmail.com>
2015-01-16 13:01:10 +0100

    Ejercicios

---
+++ empresa.php
@@ -0,0 +1,31 @@
+﻿<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8" >
+	<title></title>
+	
+</head>
+<body>
+
+	<?php
+	$idioma=$_GET['idioma'];
+	
+	switch($idioma){
+		case "espanol":
+			echo "Español";
+			break;
+		case "ingles":
+			echo "Inglés";
+			break;
+		case "frances":
+			echo "Francés";
+			break;
+		case "aleman":
+			echo "Alemán";
+			break;		
+	}
+
+	?>
+	
+</body>
+</html>
